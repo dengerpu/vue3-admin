b612daaad82ba0b7c7d76788934964bc95966a3c
dep <1649743146@qq.com>
2023-10-06 15:42:29 +0800

    feat: 项目配置: 新增eslint配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+console.log("1111-222");
+
+createApp(App).mount("#app");
