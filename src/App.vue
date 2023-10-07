0d3c4620e240ace415827f5e72632f942cf8be0a
dep <1649743146@qq.com>
2023-10-07 15:44:41 +0800

    feat: 项目集成: 配置element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { Check, Edit } from '@element-plus/icons-vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <el-button type="primary" :icon="Edit" circle />
+  <el-button type="success" :icon="Check" circle />
   <HelloWorld />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// 引入ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// @ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
