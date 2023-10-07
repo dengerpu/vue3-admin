7212fc85e87dd17b7d77d9e1330e10706bf23f4a
dep <1649743146@qq.com>
2023-10-07 10:45:37 +0800

    feat: 项目配置：prettier配置和stylelint配置

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-console.log("1111-222");
-
-createApp(App).mount("#app");
+createApp(App).mount('#app')
