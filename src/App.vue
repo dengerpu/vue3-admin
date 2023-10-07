1bc136733becc1c2b5b180abb41eeeec855e2c32
dep <1649743146@qq.com>
2023-10-07 16:18:37 +0800

    feat: 项目集成: svg配置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { Check, Edit } from '@element-plus/icons-vue'
 import HelloWorld from '@/components/HelloWorld.vue'
+console.log(import.meta.env)
 </script>
 
 <template>
   <el-button type="primary" :icon="Edit" circle />
   <el-button type="success" :icon="Check" circle />
+  <svg style="width: 32px; height: 32px">
+    <use xlink:href="#icon-test" fill="red"></use>
+  </svg>
   <HelloWorld />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // @ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+// svg
+import 'virtual:svg-icons-register'
+
 const app = createApp(App)
 
 app.use(ElementPlus, {
