24490f07461443ca789cb349b6f69cf97347048f
dep <1649743146@qq.com>
2023-10-08 12:13:25 +0800

    feat: 项目集成: axios二次封装

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import axios from 'axios'
+// import axios from 'axios'
 import { Check, Edit } from '@element-plus/icons-vue'
 import HelloWorld from '@/components/HelloWorld.vue'
 import { onMounted } from 'vue'
+import { testLogin } from './api/user/index'
 console.log(import.meta.env)
 onMounted(() => {
   let data = {
     username: 'admin',
     password: '123456'
   }
-  axios.post('/api/user/login', data).then((res) => {
-    console.log(res.data)
+  // axios.post('/api/user/login', data).then((res) => {
+  //   console.log(res.data)
+  // })
+  testLogin(data).then((res) => {
+    console.log(res)
   })
 })
 </script>
