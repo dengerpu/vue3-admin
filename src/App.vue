444479bdb54fc9d58c8df099294ec50d64af56ca
dep <1649743146@qq.com>
2023-10-08 10:53:21 +0800

    feat: 项目集成: 配置mock

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import axios from 'axios'
 import { Check, Edit } from '@element-plus/icons-vue'
 import HelloWorld from '@/components/HelloWorld.vue'
+import { onMounted } from 'vue'
 console.log(import.meta.env)
+onMounted(() => {
+  let data = {
+    username: 'admin',
+    password: '123456'
+  }
+  axios.post('/api/user/login', data).then((res) => {
+    console.log(res.data)
+  })
+})
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+// svg插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+// mock插件
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   return {
     plugins: [
       vue(),
@@ -14,6 +17,10 @@ export default defineConfig(() => {
         iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
         // Specify symbolId format
         symbolId: 'icon-[dir]-[name]'
+      }),
+      // 配置mock
+      viteMockServe({
+        localEnabled: command === 'serve'
       })
     ],
     resolve: {
