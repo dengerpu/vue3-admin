b2e134310925e20e800b869d22f1a6f429ce4490
dep <1649743146@qq.com>
2024-06-16 00:10:11 +0800

    feat: 表单二次校验

--- src/element/form/index.vue
+++ src/element/form/index.vue
@@ -1,15 +1,16 @@
 <template>
   <el-form :model="form" :rules="rules" ref="formRef" label-width="100px">
-    <el-form-item label="用户名" prop="username">
+    <el-form-item label="用户名" prop="username" :error="errorMsg">
       <el-input v-model="form.username"></el-input>
     </el-form-item>
-    <el-form-item label="邮箱" prop="email">
-      <el-input v-model="form.email"></el-input>
+    <el-form-item label="密码" prop="password" :error="errors.password">
+      <el-input type="password" v-model="form.password"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="validateForm">提交</el-button>
+      <el-button type="primary" @click="onSubmit">提交</el-button>
     </el-form-item>
   </el-form>
+  {{ errors }}
 </template>
 
 <script>
@@ -19,23 +20,28 @@ export default {
   setup() {
     const form = reactive({
       username: '',
-      email: ''
+      password: ''
     })
 
     const rules = reactive({
       username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-      email: [
-        { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-        { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
-      ]
+      password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
     })
+
+    const errors = reactive({
+      username: '用户名错误',
+      password: '密码错误'
+    })
+
+    const errorMsg = ref('123')
 
     const formRef = ref(null)
 
-    const validateForm = () => {
+    const onSubmit = () => {
       formRef.value.validate((valid) => {
         if (valid) {
-          submitForm()
+          // 发送请求到服务端
+          submitToServer()
         } else {
           console.log('表单验证失败')
           return false
@@ -43,49 +49,41 @@ export default {
       })
     }
 
-    const submitForm = () => {
-      mockBackendValidation().then((response) => {
-        if (response.valid) {
-          console.log('表单提交成功')
-        } else {
-          console.log('表单提交失败')
-          handleValidationErrors(response.errors)
+    const submitToServer = () => {
+      // 模拟服务端验证
+      setTimeout(() => {
+        // 假设服务端返回的错误信息如下
+        const serverErrors = {
+          username: '用户名已存在',
+          password: '密码不符合要求'
         }
-      })
-    }
 
-    const mockBackendValidation = () => {
-      return new Promise((resolve) => {
-        // 模拟后端校验，返回错误信息
-        setTimeout(() => {
-          resolve({
-            valid: false,
-            errors: {
-              username: '用户名已存在',
-              email: '邮箱格式不正确'
-            }
-          })
-        }, 1000)
-      })
-    }
-    const errors = reactive({
-      username: ''
-    })
-
-    const handleValidationErrors = () => {
-      errors.username = '123456'
-      // Object.keys(errors).forEach((field) => {
-      //   formRef.value.validateField(field, () => {
-      //     return new Error(errors[field])
-      //   })
-      // })
+        // 更新本地错误信息
+        errors.username = serverErrors.username
+        errors.password = serverErrors.password
+        errorMsg.value = '更新错误信息'
+        // 清除之前的验证状态
+        formRef.value.clearValidate()
+        // 手动触发错误显示
+        for (const field in errors) {
+          if (errors[field]) {
+            console.log(field)
+            formRef.value.validateField(field)
+          }
+        }
+        // 手动触发表单项验证
+        // formRef.value.validateField('username')
+        // formRef.value.validateField('password')
+      }, 1000)
     }
 
     return {
       form,
+      errors,
+      onSubmit,
       rules,
       formRef,
-      validateForm
+      errorMsg
     }
   }
 }
